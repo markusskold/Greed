14fa85a7eaeab0786afe55ba5d8bbc6a6f6234ed
markusskold <markus_skold@hotmail.com>
2015-01-23 01:17:39 +0100

    first commit

---
+++ src/com/example/greed/Die.java
@@ -0,0 +1,102 @@
+package com.example.greed;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Random;
+
+/**
+ * Created by Markus on 22.1.2015.
+ */
+public class Die extends ImageButton{
+
+    private int dieValue;
+    private boolean isLocked = false;
+
+    public Die(Context context) {
+        super(context);
+        setWhiteDieImage(1);
+    }
+
+    public Die(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWhiteDieImage(1);
+    }
+
+    public Die(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setWhiteDieImage(1);
+    }
+
+    public void setGreyDieImage(int number) {
+        if(number == 1) {
+            this.setBackgroundResource(R.drawable.grey1);
+        } else if(number == 2) {
+            this.setBackgroundResource(R.drawable.grey2);
+        } else if(number == 3) {
+            this.setBackgroundResource(R.drawable.grey3);
+        } else if(number == 4) {
+            this.setBackgroundResource(R.drawable.grey4);
+        } else if(number == 5) {
+            this.setBackgroundResource(R.drawable.grey5);
+        } else if(number == 6) {
+            this.setBackgroundResource(R.drawable.grey6);
+        }
+    }
+
+    public void setWhiteDieImage(int number) {
+        if(number == 1) {
+            this.setBackgroundResource(R.drawable.white1);
+        } else if(number == 2) {
+            this.setBackgroundResource(R.drawable.white2);
+        } else if(number == 3) {
+            this.setBackgroundResource(R.drawable.white3);
+        } else if(number == 4) {
+            this.setBackgroundResource(R.drawable.white4);
+        } else if(number == 5) {
+            this.setBackgroundResource(R.drawable.white5);
+        } else if(number == 6) {
+            this.setBackgroundResource(R.drawable.white6);
+        }
+    }
+
+    public void setRedDieImage(int number) {
+        if(number == 1) {
+            this.setBackgroundResource(R.drawable.red1);
+        } else if(number == 2) {
+            this.setBackgroundResource(R.drawable.red2);
+        } else if(number == 3) {
+            this.setBackgroundResource(R.drawable.red3);
+        } else if(number == 4) {
+            this.setBackgroundResource(R.drawable.red4);
+        } else if(number == 5) {
+            this.setBackgroundResource(R.drawable.red5);
+        } else if(number == 6) {
+            this.setBackgroundResource(R.drawable.red6);
+        }
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked() {
+        isLocked = true;
+    }
+
+    public void setUnLocked() {
+        isLocked = false;
+    }
+
+    public int getDieValue() {
+        return dieValue;
+    }
+
+    public void throwDie() {
+        Random diceRoller = new Random();
+        dieValue = diceRoller.nextInt(6) + 1;
+    }
+}
---
+++ src/com/example/greed/MyActivity.java
@@ -0,0 +1,155 @@
+package com.example.greed;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MyActivity extends Activity {
+
+    private ArrayList<Die> dice = new ArrayList<Die>();
+    private HashMap<Integer, Integer> diceMapper = new HashMap<Integer, Integer>();
+    private int totalScore = 0;
+    private int turnScore = 0;
+    private int round = 0;
+    private boolean throwAll = false;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        dice.add((Die) findViewById(R.id.die_1));
+        dice.add((Die) findViewById(R.id.die_2));
+        dice.add((Die) findViewById(R.id.die_3));
+        dice.add((Die) findViewById(R.id.die_4));
+        dice.add((Die) findViewById(R.id.die_5));
+        dice.add((Die) findViewById(R.id.die_6));
+    }
+
+
+    public void throwDice(View view) {
+        diceMapper.clear();
+        for(Die die : dice) {
+            if(die.isLocked() == false) {
+                die.throwDie();
+            } else if(throwAll == true) {
+                die.setUnLocked();
+                die.throwDie();
+            }
+            int temp = 0;
+            if(diceMapper.get(die.getDieValue()) != null) {
+                temp = diceMapper.get(die.getDieValue());
+            }
+            diceMapper.put(die.getDieValue(), temp + 1);
+        }
+        throwAll = false;
+        scoreCalculator();
+
+        Button saveButton = (Button) findViewById(R.id.save_button);
+        int nrOfLocked = 0;
+
+        if(round == 0 && turnScore < 300) {
+            for(Die die : dice) {
+                die.setWhiteDieImage(die.getDieValue());
+            }
+            turnScore = 0;
+            round = 0;
+            saveButton.setClickable(false);
+        } else if(round == 0 && turnScore >= 300){
+            for(Die die : dice) {
+                if(die.isLocked()) {
+                    die.setGreyDieImage(die.getDieValue());
+                    nrOfLocked = nrOfLocked + 1;
+                } else {
+                    die.setWhiteDieImage(die.getDieValue());
+                }
+            }
+            round = round + 1;
+            saveButton.setClickable(true);
+        } else if(round != 0 && turnScore > 0) {
+            for(Die die : dice) {
+                if(die.isLocked()) {
+                    die.setGreyDieImage(die.getDieValue());
+                    nrOfLocked = nrOfLocked + 1;
+                } else {
+                    die.setWhiteDieImage(die.getDieValue());
+                }
+            }
+            round = round + 1;
+            saveButton.setClickable(true);
+        } else if(round != 0 && turnScore == 0) {
+            for(Die die : dice) {
+                die.setWhiteDieImage(die.getDieValue());
+            }
+            turnScore = 0;
+            round = 0;
+            saveButton.setClickable(false);
+        }
+        if(nrOfLocked == 6) {
+            throwAll = true;
+        }
+    }
+
+    public void save(View view) {
+        totalScore = totalScore + turnScore;
+        TextView totalScoreView = (TextView) findViewById(R.id.textView);
+        totalScoreView.setText(getResources().getString(R.string.total_score) + totalScore);
+        view.setClickable(false);
+        round = 0;
+    }
+
+
+    protected void scoreCalculator() {
+        for(int i = 1; i < 7; i++) {
+            if(diceMapper.get(i) != null) {
+                if(diceMapper.get(i) >= 3) {
+                    if(i == 1) {
+                        turnScore = turnScore + 1000;
+                        turnScore = turnScore + ((diceMapper.get(i) - 3)*100);
+                        diceLocker(i, diceMapper.get(i));
+                    } else if(i == 5) {
+                        turnScore = turnScore + (i*100);
+                        turnScore = turnScore + ((diceMapper.get(i) - 3)*50);
+                        diceLocker(i, diceMapper.get(i));
+                    }  else {
+                        turnScore = turnScore + (i*100);
+                        diceLocker(i, 3);
+                    }
+                } else {
+                    if(i == 1) {
+                        turnScore = turnScore + (diceMapper.get(i)*100);
+                        diceLocker(i, diceMapper.get(i));
+                    } else if(i == 5) {
+                        turnScore = turnScore + (diceMapper.get(i)*50);
+                        diceLocker(i, diceMapper.get(i));
+                    }
+                }
+                if(diceMapper.size() == 6) {
+                    diceLocker(i, 1);
+                }
+            }
+        }
+        if(diceMapper.size() == 6) {
+            turnScore = turnScore + 1000;
+        }
+    }
+
+    protected void diceLocker(int diceValue, int numberOfDice) {
+        int diceSet = 0;
+        for(Die die : dice) {
+            if(die.getDieValue() == diceValue && die.isLocked() == false) {
+                if(diceSet < numberOfDice) {
+                    die.setLocked();
+                    diceSet = diceSet + 1;
+                }
+            }
+        }
+    }
+}
